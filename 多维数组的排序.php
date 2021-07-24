946b7a8a857904a740109db2ad834efb9b15ecb9
啭地的猫 <782101031@qq.com>
2021-07-24 10:34:07 +0800

    Update 多维数组的排序.php

--- 多维数组的排序.php
+++ 多维数组的排序.php
@@ -65,3 +65,24 @@ Array
 
 )
  */
+
+    /**
+     * 创建多维数组
+     * $keys = ['a', 'b', 'c']
+     * 输出 $original['a']['b']['c'] = $value
+     * @param array $keys 键值
+     * @param array $original 想动态创建的多维数组名
+     * @param null $value 创建后赋值，默认为null
+     * @date 2021/7/24 10:19 上午
+     * @author sky.he 782101031@qq.com
+     */
+    public function multiArrayCreate(array $keys, &$original = [], $value = null)
+    {
+        $ref  = &$original;
+        $keys = ['a', 'b', 'c'];
+        foreach ($keys as $key) {
+            $ref[$key] = [];
+            $ref = &$ref[$key];
+        }
+        $ref = $value;
+    }
